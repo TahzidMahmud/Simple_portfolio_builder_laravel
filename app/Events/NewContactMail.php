3413c824dd98ec1981857e357e90ac3783505432
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2020-12-03 00:19:02 +0600

    done

---
+++ app/Events/NewContactMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NewContactMail implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $id;
+
+    public function __construct($id)
+    {
+        $this->id=$id;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('contact-mail'.$this->id);
+    }
+    public function broadcastWith()
+    {
+        return ['message'=>$this->message];
+    }
+}
