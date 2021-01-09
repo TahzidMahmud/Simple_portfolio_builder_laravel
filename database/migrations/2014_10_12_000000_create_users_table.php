0e197d17a95182bf290e6f52007ea608c9a57cc6
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-01-09 12:49:10 +0600

    migration modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('phone_no')->nullable();
             $table->String('address')->nullable();
             $table->json('social_links')->nullable();
